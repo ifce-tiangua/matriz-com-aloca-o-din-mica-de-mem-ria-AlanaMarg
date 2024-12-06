062458870b5e5a4da157ca7f7cefae47a7f25139
Alana <alana.pereiraa61@gmail.com>
2024-12-06 00:03:41 +0000

    crt

--- main.c
+++ main.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int** matriz_creat(int linhas, int colunas);
+void matriz_read(int** matriz, int linhas, int colunas);
+void matriz_show(int** matriz, int linhas, int colunas);
+void matriz_destroy(int **matriz, int linhas);
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int** matriz, linhas, colunas;
+    scanf("%d %d", &linhas, &colunas);
+
+    if(linhas <= 0 || colunas <= 0) {
+        printf("[matriz vazia]\n");
+        return 0;
+    } else {
+    matriz = matriz_creat(linhas, colunas);
+    matriz_read(matriz, linhas, colunas);
+    matriz_show(matriz, linhas, colunas);
+    matriz_destroy(matriz, linhas);
+    }
 
     return 0;
+}
+
+int** matriz_creat(int linhas, int colunas) {
+    int **matriz;
+
+    matriz = (int**)malloc(linhas * sizeof(int*));
+    if(matriz != NULL)
+        for(int i = 0; i < linhas; i++) {
+             matriz[i] = (int*)malloc(colunas * sizeof(int));
+        }
+    return matriz;
+}
+
+void matriz_read(int** matriz, int linhas, int colunas) {
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void matriz_show(int** matriz, int linhas, int colunas) {
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void matriz_destroy(int **matriz, int linhas) {
+    for(int i = 0; i < linhas; i++){
+        free(matriz[i]);
+    }
+    free(matriz);
 }
